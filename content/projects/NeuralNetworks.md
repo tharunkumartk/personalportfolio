e35938e3150da146a72cb0095371af5fbd5ae14f
Tharun Kumar Tiruppali Kalidoss <tharun@Kalidosss-MacBook-Air.local>
2022-01-28 11:51:26 -0600

    Updated Research and Projects

---
+++ content/projects/NeuralNetworks.md
@@ -0,0 +1,14 @@
+---
+date: '2017-12-01'
+title: 'Neural Networks'
+github: ''
+external: 'https://tools.applemusic.com/en-us'
+tech:
+  - Python
+  - Tensorflow
+  - Keras
+company: 'Independent Study and Mentorship'
+showInProjects: true
+---
+
+Research regarding the viablity of Neural Networks for real-life application, along with a concise description of their inner workings. 
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Tharun Kumar',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Tharun Kumar Tiruppali Kalidoss is a Data Scientist that specializes in utilizing data to provide insightful predictions.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
