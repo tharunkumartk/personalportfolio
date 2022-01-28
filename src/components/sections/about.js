e60f0a8997b3a4ee62a0335c6c188e4c8ad75639
Tharun Kumar Tiruppali Kalidoss <tharun@Kalidosss-MacBook-Air.local>
2022-01-28 09:47:54 -0600

    about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'Python', 'React', 'Java', 'SciKit-Learn', 'Natural Language Toolkit, Tensorflow'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +136,15 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Yooooo its ma boy Nivedh, name is Tharun Kumar and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              My name is Tharun Kumar, and I enjoy analyzing the vast 40 zettabytes of the internet to make viable predictions that have a meaningful impact on society. My interest in Data Science started when I was seven years old, when I attempted to emulate Sherlock Holmes in predicting human behavior through Artificial Intelligence. Although clueless at the time, I've grown significantly in the field of Computer Science through college-level courses and real-world experience. 
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="https://transgti.com/">a transportation intelligence company</a>,{' '}
+              <a href="https://intuitiontx.com">a start-up tutoring business</a>,{' '}and{' '}
+              <a href="https://www.apple.com/">a mentorship</a>. My
+              main focus these days is building AI tools that increase accessiblity of medical care at my school's Independent Study and Mentorship program.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
