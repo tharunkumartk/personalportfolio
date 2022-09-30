f313dd555a277fd6c19c00f3f26a959fb6ac2837
TharunTK <tharun.tiruppali@gmail.com>
2022-09-30 11:52:35 -0400

    updated resume

--- src/components/nav.js
+++ src/components/nav.js
@@ -173,7 +173,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="https://drive.google.com/file/d/1DvXH0-isUvmhfWZaPpwtnORrvt5rtNZs/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="https://drive.google.com/file/d/142kLUTvygMt9NVVR1F71athcjoIDccVy/view?usp=sharing" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
